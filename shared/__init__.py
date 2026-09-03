679b37fbd0608e64c25bb7cbbfd4c224cb9165bb
Bekmurod <bekmurodgofurov0000@gmail.com>
2026-09-03 14:42:18 +0500

    feat: add shared frozen contracts and CI workflow with tests

---
+++ shared/__init__.py
@@ -0,0 +1,23 @@
+from shared.contracts import (
+    Aspect,
+    AspectHit,
+    AspectResponse,
+    AspectResult,
+    ReviewIn,
+    ScoreRequest,
+    Sentiment,
+    SentimentResponse,
+    SentimentResult,
+)
+
+__all__ = [
+    "Sentiment",
+    "Aspect",
+    "ReviewIn",
+    "ScoreRequest",
+    "SentimentResult",
+    "SentimentResponse",
+    "AspectHit",
+    "AspectResult",
+    "AspectResponse",
+]
